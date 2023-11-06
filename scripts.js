7f59cba241f4bce4d7d983ad143fe9e96c060135
tid213 <tid2136141@gmail.com>
2023-11-06 16:15:34 -0700

    added scroll effect and changed tabs to nav links

--- scripts.js
+++ scripts.js
@@ -1,14 +1,15 @@
 const sections = document.querySelectorAll("section");
-const navLi = document.querySelectorAll("nav .container ul li");
+const navLi = document.querySelectorAll("nav .tabs-div ul li");
+console.log(sections[0])
 window.onscroll = () => {
   var current = "";
 
   sections.forEach((section) => {
     const sectionTop = section.offsetTop;
-    if (scrollY >= sectionTop - 60) {
+    console.log(sectionTop)
+    if (scrollY >= sectionTop - 200 ) {
       current = section.getAttribute("id"); }
   });
-
   navLi.forEach((li) => {
     li.classList.remove("active");
     if (li.classList.contains(current)) {
