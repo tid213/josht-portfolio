b8023281df311189fcfaad51ac8daddfaf2db99b
tid213 <tid2136141@gmail.com>
2023-11-06 16:47:37 -0700

    added tab change on scroll

--- scripts.js
+++ scripts.js
@@ -1,18 +1,26 @@
 const sections = document.querySelectorAll("section");
 const navLi = document.querySelectorAll("nav .tabs-div ul li");
-console.log(sections[0])
+const navLine = document.querySelectorAll("nav .tabs-div ul li .line-extend");
+
+console.log(navLine)
 window.onscroll = () => {
   var current = "";
 
   sections.forEach((section) => {
     const sectionTop = section.offsetTop;
-    console.log(sectionTop)
     if (scrollY >= sectionTop - 200 ) {
       current = section.getAttribute("id"); }
   });
   navLi.forEach((li) => {
     li.classList.remove("active");
     if (li.classList.contains(current)) {
+        navLine.forEach((line) => {
+            line.classList.remove("active");
+            if(line.classList.contains(current)){
+                line.classList.add("active")
+                console.log(navLine)
+            }
+        })
       li.classList.add("active");
     }
   });
